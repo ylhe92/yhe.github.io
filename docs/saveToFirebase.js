660b97da7da2b8d3dba0ee17d379eea535cd4f3f
ylhe92 <ylhepower@gmail.com>
2020-08-26 17:14:44 +0100

    updates

--- docs/saveToFirebase.js
+++ docs/saveToFirebase.js
@@ -12,29 +12,5 @@ function saveToFirebase(email) {
         });
 }
 
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.19.0/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.19.0/firebase-analytics.js"></script>
-
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyD5qGknO7A_nvl5bjlVsLuIvWaAiZ72gSk",
-    authDomain: "ylhe-6a941.firebaseapp.com",
-    databaseURL: "https://ylhe-6a941.firebaseio.com",
-    projectId: "ylhe-6a941",
-    storageBucket: "ylhe-6a941.appspot.com",
-    messagingSenderId: "1042102527429",
-    appId: "1:1042102527429:web:e52718a9d17a70eca657ac",
-    measurementId: "G-HKC4GKZ7W9"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
-
 const email = document.getElementById('inputEmail');
 saveToFirebase(email);
